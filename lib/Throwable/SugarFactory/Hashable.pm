240675f75d001a9927af0d5e6232e0c0d574a6ae
Christian Walde <walde.christian@gmail.com>
2021-12-02 17:20:03 +0100

    skip non-attribute method new_with_previous on converting Throwable to hash

--- lib/Throwable/SugarFactory/Hashable.pm
+++ lib/Throwable/SugarFactory/Hashable.pm
@@ -23,7 +23,7 @@ sub to_hash {
     my @base_methods = qw( error namespace description previous_exception );
     my %skip_methods = map { $_ => 1 } @base_methods,
       qw( BUILDALL BUILDARGS DEMOLISHALL DOES after around before does extends
-      has meta new throw previous_exception to_hash with );
+      has meta new throw previous_exception to_hash with new_with_previous );
     my $methods = Class::Inspector->methods( ref $self, 'public' );
     my %data = map { $_ => $self->$_ } grep { !$skip_methods{$_} } @{$methods};
     my %out = ( data => \%data, map { $_ => $self->$_ } @base_methods );
